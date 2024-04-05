6781808fb5be5d93ce799969158318791909387d
Lee Tze Wei, Alex Joel <lee.tze.wei.alex.joel@gmail.com>
2024-04-06 01:58:19 +0800

    feat: offset draggable downwards

--- lib/src/utils/event_grid.dart
+++ lib/src/utils/event_grid.dart
@@ -189,7 +189,7 @@ class EventDrawProperties {
               Positioned(
                 left: 0,
                 right: 0,
-                bottom: 0,
+                bottom: -10,
                 height: 40,
                 child: resizeGestureDetector,
               ),
